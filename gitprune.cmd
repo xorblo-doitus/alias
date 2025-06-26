4f8eb7f6955a1525a674a8a5c7cef4e3414ce8d0
Patou <75997617+xorblo-doitus@users.noreply.github.com>
2025-06-26 14:10:22 +0200

    Fix command showing "()" on a line

--- gitprune.cmd
+++ gitprune.cmd
@@ -9,7 +9,7 @@ IF /I NOT "%1"=="--yes" (
 )
 
 
-FOR /F %%B IN ('git branch --merged main --no-color --format=%%(refname:short^)') DO (
+@FOR /F %%B IN ('git branch --merged main --no-color --format=%%(refname:short^)') DO (
 	@IF /I NOT "%%B"=="main" (
 		IF /I "%1"=="--yes" (
 			echo Archiving %%B
